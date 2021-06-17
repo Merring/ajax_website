ec19078b576857a1ffee4437650c856f2c53a116
Merring <mindy000@naver.com>
2021-06-17 20:19:46 +0900

    first_commit

---
+++ js/snapshots_add.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+
+    //2차 배열 패턴 = ["이미지", "타이틀"];
+    var $sn_arr = [
+        ["cont_07.jpg", "Snapshot_07"],
+        ["cont_08.jpg", "Snapshot_08"],
+        ["cont_09.jpg", "Snapshot_09"],
+        ["cont_10.jpg", "Snapshot_10"],
+        ["cont_11.jpg", "Snapshot_11"],
+        ["cont_12.jpg", "Snapshot_12"],
+        ["cont_13.jpg", "Snapshot_13"],
+        ["cont_14.jpg", "Snapshot_14"]
+    ];
+
+    var $snAdd_box = `
+    <div class="sn_box">
+        <div class="dark">
+            <h4>Snapshot_01</h4>
+        </div>
+    </div>
+    `;
+
+    for(i in $sn_arr){
+        console.log(i);
+        $(".add_box").append($snAdd_box);
+    };
+
+    $(".add_box .sn_box").each(function(index){
+        $(this).css("background-image", "url(./img/"+$sn_arr[index][0]+")");
+        $(this).find("h4").text($sn_arr[index][1]);
+    });
+
+
+
+
+
+
+});
